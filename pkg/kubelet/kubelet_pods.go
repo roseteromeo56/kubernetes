f9266efc0700fe470b2753c651b2e71d2354731c
datadog-bits <263423550+datadog-bits@users.noreply.github.com>
2026-07-03 13:26:53 +0000

    Harden kubelet logs response headers

--- pkg/kubelet/kubelet_pods.go
+++ pkg/kubelet/kubelet_pods.go
@@ -2364,6 +2364,8 @@ func (kl *Kubelet) convertToAPIContainerStatuses(pod *v1.Pod, podStatus *kubecon
 // ServeLogs returns logs of current machine.
 func (kl *Kubelet) ServeLogs(w http.ResponseWriter, req *http.Request) {
 	// TODO: allowlist logs we are willing to serve
+	w.Header().Set("Content-Type", "text/plain;charset=UTF-8")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
 	kl.logServer.ServeHTTP(w, req)
 }
 
--- pkg/kubelet/kubelet_pods_test.go
+++ pkg/kubelet/kubelet_pods_test.go
@@ -21,6 +21,8 @@ import (
 	"errors"
 	"fmt"
 	"net"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"path/filepath"
 	"reflect"
@@ -358,6 +360,27 @@ func TestRunInContainer(t *testing.T) {
 		assert.Equal(t, "foo", string(actualOutput), "(testError=%v) output", testError)
 		assert.Equal(t, err, testError, "(testError=%v) err", testError)
 	}
+}
+
+func TestServeLogsSetsSecureContentHeaders(t *testing.T) {
+	tmpDir, err := os.MkdirTemp("", "kubelet-serve-logs")
+	require.NoError(t, err)
+	defer os.RemoveAll(tmpDir)
+
+	err = os.WriteFile(filepath.Join(tmpDir, "malicious.html"), []byte("<script>alert('xss')</script>"), 0644)
+	require.NoError(t, err)
+
+	kubelet := &Kubelet{}
+	kubelet.logServer = http.StripPrefix("/logs/", http.FileServer(http.Dir(tmpDir)))
+
+	req := httptest.NewRequest(http.MethodGet, "/logs/malicious.html", nil)
+	resp := httptest.NewRecorder()
+
+	kubelet.ServeLogs(resp, req)
+
+	assert.Equal(t, http.StatusOK, resp.Code)
+	assert.Equal(t, "text/plain;charset=UTF-8", resp.Header().Get("Content-Type"))
+	assert.Equal(t, "nosniff", resp.Header().Get("X-Content-Type-Options"))
 }
 
 type testServiceLister struct {
