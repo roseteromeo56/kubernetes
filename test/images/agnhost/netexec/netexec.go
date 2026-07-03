0a48e801772d0b56584f5b76d61f014f31a9957e
datadog-bits <263423550+datadog-bits@users.noreply.github.com>
2026-07-03 13:21:19 +0000

    Harden netexec dial error responses

--- test/images/agnhost/netexec/netexec.go
+++ test/images/agnhost/netexec/netexec.go
@@ -402,11 +402,11 @@ func dialHandler(w http.ResponseWriter, r *http.Request) {
 		tries, err = strconv.Atoi(tryParam)
 	}
 	if err != nil {
-		http.Error(w, fmt.Sprintf("tries parameter is invalid. %v", err), http.StatusBadRequest)
+		http.Error(w, "tries parameter is invalid", http.StatusBadRequest)
 		return
 	}
 	if len(request) == 0 {
-		http.Error(w, fmt.Sprintf("request parameter not specified. %v", err), http.StatusBadRequest)
+		http.Error(w, "request parameter not specified", http.StatusBadRequest)
 		return
 	}
 
